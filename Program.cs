218ffc321e2c7d96ad0af91581a57bc66e214094
veronica <timurkkq@gmail.com>
2025-09-08 14:43:56 +0300

    end

--- Program.cs
+++ Program.cs
@@ -5,8 +5,63 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            //Console.WriteLine("Hello world");
+            //Console.ReadLine();
+            //double a = 10;
+            //double b = 3;
+            //double c = a/b;
+            //Console.WriteLine();
+            //double x = 10 / 3;
+            //Console.WriteLine(x);
+            //int x1 = 4;
+            //int y1 = ++x1;
+            //Console.WriteLine($"x1 {x1}, y1 {y1}");
 
+            //int x2 = 4;
+            //int y2 = x2++;
+            //Console.WriteLine($"x1 {x2}, y1 {y2}");
+
+            //Console.WriteLine(10 / 5 * 2);
+
+            //bool x = (5 > 6) | (4 < 7);
+            //bool x2 = (5 > 6) && (4 < 7);
+            //Console.WriteLine(x);
+
+            //double number = 5.325624;
+            //Console.WriteLine($"{number:F2}");
+            //Console.WriteLine($"{number:C}");
+            //Console.WriteLine($"{number:P}");
+
+            //Console.Write("integer ")                                     ;
+            //int s = int.Parse(Console.ReadLine());
+            //if (s > 0)
+            //{
+            //    Console.WriteLine("big");
+            //}
+            //else if (s == 0)
+            //{
+            //    Console.WriteLine("small");
+            //}
+
+
+            //int x = 3;
+            //int y = 2;
+            //int z = (x > y) ? x + y : (x - y);
+            //Console.Write("ваша любимая игра ");
+            //string fav = Console.ReadLine();
+
+            //switch (fav)
+            //{
+            //case "Dota2":
+            //        Console.WriteLine("Good");
+            //        goto case "factorio";
+            //        break;
+            //case "factorio":
+            //        Console.WriteLine("ХАРОООООООООШ");
+            //        break;
+            //default:
+            //        Console.WriteLine("Не найдено");
+            //}
         }
     }
 }
